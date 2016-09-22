435b367f291913a6cb36acef7932cb69693d0b13
Christian Brauner <christian.brauner@canonical.com>
2016-09-22 14:27:11 +0200

    explore
    Signed-off-by: Christian Brauner <christian.brauner@canonical.com>

--- lxd/migrate.go
+++ lxd/migrate.go
@@ -216,7 +216,6 @@ func (s *migrationSourceWs) Connect(op *operation, r *http.Request, w http.Respo
 	}
 
 	c, err := shared.WebsocketUpgrader.Upgrade(w, r, nil)
-	shared.LogWarnf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 	if err != nil {
 		return err
 	}
